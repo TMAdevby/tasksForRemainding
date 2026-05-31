dbb65362b2afabf046eb9400b6fc7e38eba98b2d
TMAdevby <mtipun@mail.ru>
2026-05-31 12:28:16 +0300

    filesIO_2

--- src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main1.java
+++ src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main1.java
@@ -1,6 +1,6 @@
 package com.example.FilesIO.first_managerOfConfigurations;
 
-import java.io.File;
+import java.io.*;
 import java.time.LocalDateTime;
 
 public class Main1 {
@@ -8,6 +8,13 @@ public class Main1 {
     private static StringBuilder logs = new StringBuilder();
 
     public static void main(String[] args) {
+
+
+
+
+
+
+
 
     }
 
@@ -20,8 +27,38 @@ public class Main1 {
             logs.append("Directory " + path + " has been created in time " + createTime + ".\n");
         }
         else {
-            logs.append(" Directory " + path + " was not created, try agen .\n");
+            logs.append("Directory " + path + " was not created, try agen .\n");
         }
+    }
+
+    public static void createFile(String path) {
+        File file = new File(path);
+
+        try {
+            file.createNewFile()
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        LocalDateTime createTime = LocalDateTime.now();
+
+        if(file.isFile() && file.exists()){
+            logs.append("File " + path + " has been created in time " + createTime + ".\n");)
+        }
+        else {
+            logs.append("File " + path + " was not created, try agen .\n");
+        }
+    }
+
+    public static void writeLogs(){
+        try(BufferedWriter br = new BufferedWriter(
+                new FileWriter("D:\\Games\\FilesExperements\\temp\\temp.txt"))){
+            for (int i = 0; i < logs.length(); i++) {
+                br.write(logs.charAt(i));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Logs have been written");
     }
 
 

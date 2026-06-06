d3aae6a2968dfa70e5929dba920d614a61584074
TMAdevby <mtipun@mail.ru>
2026-06-06 22:33:25 +0300

    filesIO_13

--- src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main3.java
+++ src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main3.java
@@ -3,6 +3,9 @@ package com.example.FilesIO.first_managerOfConfigurations;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main3 {
@@ -11,7 +14,6 @@ public class Main3 {
         createText();
         createText();
         readDiary();
-
     }
 
     public static File createDiaryFile(String fileName){
@@ -37,7 +39,7 @@ public class Main3 {
 
         try(BufferedWriter bw = new BufferedWriter(new FileWriter("diary.txt",true))){
             String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            bw.write("[ " + timestamp + " ]");
+            bw.write("[" + timestamp + "] " + line);
             bw.newLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -46,16 +48,37 @@ public class Main3 {
 
     public static void readDiary() {
         try(BufferedReader br = new BufferedReader(new FileReader("diary.txt"))) {
+            LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
             String line;
             int i = 0;
+            int maxLengthI = 0;
+            int maxLength = 0;
             while((line = br.readLine()) != null){
                 i++;
+                if(line.length() > maxLength){
+                    maxLength = line.length();
+                    maxLengthI = i;
+                }
+                String [] str = line.split(" ");
+                map.put(i,str.length);
                 System.out.println(i + ". " + line);
+
             }
+            System.out.println("Количество записей = " + i);
+            System.out.println("Самая длинная запись № " + maxLengthI);
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+                System.out.println(entry.getKey() + " запись " + entry.getValue() + " слов");
+            }
+
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
+
+
 }

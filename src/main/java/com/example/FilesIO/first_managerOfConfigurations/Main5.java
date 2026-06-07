157d7b5314cf0bb95cb9c682d0dffeade57731f3
TMAdevby <mtipun@mail.ru>
2026-06-07 14:13:15 +0300

    filesIO_19

--- src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main5.java
+++ src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main5.java
@@ -2,13 +2,13 @@ package com.example.FilesIO.first_managerOfConfigurations;
 
 import com.sun.jdi.PathSearchingVirtualMachine;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Main5 {
     public static void main(String[] args) {
         createFile("data.bin");
-
+        writeToFile();
+        readFromFile();
     }
 
     public static void createFile(String name){
@@ -23,5 +23,31 @@ public class Main5 {
         }
     }
 
-    pu
+    public static void writeToFile(){
+        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream("data.bin"))){
+            dos.writeInt(100);
+            dos.writeDouble(3.14);
+            dos.writeBoolean(true);
+            dos.writeUTF("Привет");
+            System.out.println("Записано");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void readFromFile() {
+        try(DataInputStream dis = new DataInputStream(new FileInputStream("data.bin"))) {
+            System.out.println(dis.readInt());
+            System.out.println(dis.readDouble());
+            System.out.println(dis.readBoolean());
+            System.out.println(dis.readUTF());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

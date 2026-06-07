ad5f82a151f6abfcf891db1359d0033ab5a0ea4e
TMAdevby <mtipun@mail.ru>
2026-06-07 15:29:37 +0300

    filesIO_20

---
+++ src/main/java/com/example/FilesIO/first_managerOfConfigurations/Main6.java
@@ -0,0 +1,76 @@
+package com.example.FilesIO.first_managerOfConfigurations;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main6 {
+
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createFile("nio");
+        writeToFile("nio", "Привет");
+        writeToFile("nio", "Как дела?");
+        writeToFile("nio", "Как успехи?");
+        list = readFile("nio");
+        copyFile("nio","copy");
+
+    }
+
+    public static void createFile(String name){
+        Path file = Paths.get(name);
+        if(!Files.exists(file)){
+            try {
+                Files.createFile(Path.of(name));
+                System.out.println("Файл " + name + " создан");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void writeToFile(String file, String line) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+            bw.write(line);
+            bw.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String> readFile(String name){
+        List<String> list = new ArrayList<>();
+        try {
+            list = Files.readAllLines(Path.of(name));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for(String str: list){
+            System.out.println(str);
+        }
+        return list;
+    }
+
+    public static void copyFile(String name, String newName){
+        Path file = Paths.get(name);
+        Path destFile = Paths.get(name);
+        if(Files.exists(file) && !Files.exists(destFile)){
+            try {
+                Files.copy(Path.of(name), Path.of(newName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else{
+            System.out.println("Файл не существует");
+        }
+    }
+
+    public static
+
+}

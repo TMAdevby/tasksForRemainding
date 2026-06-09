654ac8d0e20462b6a10dea8eb13904a2a2f3e06d
TMAdevby <mtipun@mail.ru>
2026-06-09 19:54:51 +0300

    multithreading5

---
+++ src/main/java/com/example/multithresding/part01_create_methods/Main4.java
@@ -0,0 +1,21 @@
+package com.example.multithresding.part01_create_methods;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int i = 1; i < 6; i++) {
+                System.out.printf("Загрузка ... [%d]\n", i);
+            }
+        });
+
+        System.out.println("Начало загрузки");
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+        System.out.println("Загрузка завершена, главный поток продолжает работу");
+    }
+}

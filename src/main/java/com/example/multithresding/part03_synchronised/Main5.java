a66ff844ebfc696057d88e73709e702e18dccc09
TMAdevby <mtipun@mail.ru>
2026-06-13 21:56:39 +0300

    multithreading17

--- src/main/java/com/example/multithresding/part03_synchronised/Main5.java
+++ src/main/java/com/example/multithresding/part03_synchronised/Main5.java
@@ -4,19 +4,36 @@ public class Main5 {
     public static void main(String[] args) {
         SafeLogger safeLogger = new SafeLogger();
 
-        Thread
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                safeLogger.log(Thread.currentThread().getName(),"Сообщение 1");
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                safeLogger.log(Thread.currentThread().getName(),"Сообщение 2");
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                safeLogger.log(Thread.currentThread().getName(),"Сообщение 3");
+            }
+        });
     }
 }
 
-class SafeLogger{
+class SafeLogger {
     private final Object logLock = new Object();
 
-    public void log(String threadName, String message){
-        synchronized (logLock){
+    public void log(String threadName, String message) {
+        synchronized (logLock) {
             System.out.printf("[%s] начал запись: %s -> Thread.sleep(50) -> [%s] закончил запись: %s"
-                , Thread.currentThread().getName(), message ,Thread.currentThread().getName(), message);
+                    , threadName, message, threadName, message);
         }
     }
+}
 
 
 

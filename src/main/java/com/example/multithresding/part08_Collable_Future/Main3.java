384123505b3ab4a242e82251ce945d4cb398c345
TMAdevby <mtipun@mail.ru>
2026-06-20 14:08:11 +0300

    multithreading28

--- src/main/java/com/example/multithresding/part08_Collable_Future/Main3.java
+++ src/main/java/com/example/multithresding/part08_Collable_Future/Main3.java
@@ -1,20 +1,22 @@
 package com.example.multithresding.part08_Collable_Future;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        executorService.submit(new RaceWorker("Заяц", 1));
-        executorService.submit(new RaceWorker("Лис", 2));
-        executorService.submit(new RaceWorker("Черепаха", 3));
+        Future<String> future1 = executorService.submit(new RaceWorker("Заяц", 1));
+        Future<String> future2 = executorService.submit(new RaceWorker("Лис", 2));
+        Future<String> future3 = executorService.submit(new RaceWorker("Черепаха", 3));
 
         executorService.shutdown();
 
+        String result1 = future1.get();
+        String result2 = future2.get();
+        String result3 = future3.get();
 
+        System.out.println("Победители в порядке финиша : " + result1 + "," + result2 + "," + result3);
     }
 }
 

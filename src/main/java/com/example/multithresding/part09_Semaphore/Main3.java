bc8d07ccef91c36db532deba3b73cdf6b6c23683
TMAdevby <mtipun@mail.ru>
2026-07-01 21:49:11 +0300

    multithreading34

--- src/main/java/com/example/multithresding/part09_Semaphore/Main3.java
+++ src/main/java/com/example/multithresding/part09_Semaphore/Main3.java
@@ -1,17 +1,31 @@
 package com.example.multithresding.part09_Semaphore;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class Main3 {
     public static void main(String[] args) {
+        FittingRoom fr = new FittingRoom();
 
+        ExecutorService executorService = Executors.newFixedThreadPool(7);
+
+        executorService.execute(new Buyer("Иван", 3 , fr));
+        executorService.execute(new Buyer("Мирон", 5 , fr));
+        executorService.execute(new Buyer("Матвей", 6 , fr));
+        executorService.execute(new Buyer("Елена", 5 , fr));
+        executorService.execute(new Buyer("Ирина", 8 , fr));
+        executorService.execute(new Buyer("Татьяна", 4 , fr));
+        executorService.execute(new Buyer("Екатерина", 6 , fr));
+
+        executorService.shutdown();
     }
 }
 
 class FittingRoom {
     private final Semaphore semaphore = new Semaphore(3);
 
-    private void tryOn(String clientName, int seconds){
+    public void tryOn(String clientName, int seconds){
         try {
             semaphore.acquire();
             System.out.println(clientName + " вошел в кабинку, примеряет " + seconds + " секунд.");
@@ -19,14 +33,28 @@ class FittingRoom {
             System.out.println(clientName + " вышел из кабинки.");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-        }finally {
+        } finally {
             semaphore.release();
         }
     }
 }
 
-class Buyer {
+class Buyer implements Runnable{
 
+    private String name;
+    private int sec;
+    private FittingRoom fr;
+
+    public Buyer(String name, int sec, FittingRoom fr) {
+        this.name = name;
+        this.sec = sec;
+        this.fr = fr;
+    }
+
+    @Override
+    public void run() {
+        fr.tryOn(name,sec);
+    }
 }
 
 
---
+++ src/main/java/com/example/multithresding/part10_CountDownLanch/Main1.java
@@ -0,0 +1,32 @@
+package com.example.multithresding.part10_CountDownLanch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int workersCount = 5;
+        CountDownLatch countDownLatch = new CountDownLatch(workersCount);
+
+        for (int i = 1; i <= workersCount; i++) {
+            final int id = i;
+            new Thread(() ->{
+            try {
+                    Thread.sleep((int) (Math.random() * 2000));
+                System.out.println("Воркер " + id + " завершил работу.");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                countDownLatch.countDown();
+            }
+            }).start();
+        }
+
+        System.out.println("Главный поток ждет завершения всех потоков...");
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Главный поток завершил работу");
+    }
+}

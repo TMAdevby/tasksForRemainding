6f3715c2a77a767de5fafc6f8597af590d97a37a
TMAdevby <mtipun@mail.ru>
2026-07-09 20:47:09 +0300

    multithreading43

---
+++ src/main/java/com/example/multithresding/part13_ConcurentHashMap/Main1.java
@@ -0,0 +1,57 @@
+package com.example.multithresding.part13_ConcurentHashMap;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main1 {
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentHashMap <String, Integer> concurrentHashMap = new ConcurrentHashMap();
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        Thread thread1 = new Thread(() -> {
+
+
+
+            for (int i = 1; i < 11; i++) {
+                System.out.println("Click");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                concurrentHashMap.put("Click " + i,counter.incrementAndGet());
+            }
+
+        });
+
+        Thread thread2 = new Thread(() -> {
+
+            AtomicInteger exceptionCounter = new AtomicInteger(0);
+
+            for (int i = 1; i < 7; i++) {
+                System.out.println("Exception");
+                try {
+                    Thread.sleep(800);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                concurrentHashMap.put("Eхception " + i ,counter.incrementAndGet());
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        for (Map.Entry<String, Integer> item : concurrentHashMap.entrySet()) {
+            System.out.println("Событие " + item.getKey() + " Порядковый номер " + item.getValue());
+        }
+
+        System.out.println("Конец");
+    }
+}
+

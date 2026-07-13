57658acbb78873f924c132a8ad832e0c2680b766
TMAdevby <mtipun@mail.ru>
2026-07-13 22:04:19 +0300

    multithreading51

--- src/main/java/com/example/multithresding/part14_CopyOnWriteArrayList/Main2.java
+++ src/main/java/com/example/multithresding/part14_CopyOnWriteArrayList/Main2.java
@@ -3,25 +3,42 @@ package com.example.multithresding.part14_CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main2 {
-    public static void main(String[] args) {
-        CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
+    public static void main(String[] args) throws InterruptedException {
+        CopyOnWriteArrayList <String> copyOnWriteArrayList = new CopyOnWriteArrayList();
 
         Thread thread1 = new Thread(() -> {
             for (int i = 1; i < 31 ; i++) {
                 String name = "User " + i;
+                copyOnWriteArrayList.add(name);
+            }
+        });
+
+        Thread thread1a = new Thread(() -> {
+            System.out.println("--- Поток 1А начал итерацию ---");
+            try {
+                thread1.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            for (String user : copyOnWriteArrayList) {
+                System.out.println("Поток 1А видит: " + user);
                 try {
-                    Thread.sleep(100);
-                    copyOnWriteArrayList.add(name);
+                    Thread.sleep(500); // Спим, чтобы Поток Б успел изменить список
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                    return;
                 }
             }
+            System.out.println("--- Поток 1А закончил итерацию ---");
         });
 
         Thread thread2 = new Thread(() -> {
             try {
-                Thread.sleep(2500);
+                thread1.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Thread.sleep(250);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 return;
@@ -40,7 +57,19 @@ public class Main2 {
             }
         });
 
+        thread1.start();
+        thread1a.start();
+        thread2.start();
 
+        thread1.join();
+        thread1a.join();
+        thread2.join();
+
+        for(Object name : copyOnWriteArrayList){
+            System.out.println(name);
+        }
+
+        System.out.println("The end");
 
     }
 

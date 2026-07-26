4e4feceacf27a79e714247228be6a03b1a18fcda
TMAdevby <mtipun@mail.ru>
2026-07-26 19:12:49 +0300

    mock6

--- src/test/java/Mock/first/TransactionProcessorTest.java
+++ src/test/java/Mock/first/TransactionProcessorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.stubbing.BaseStubbing;
 
 public class TransactionProcessorTest {
 
@@ -27,4 +28,25 @@ public class TransactionProcessorTest {
             Mockito.verify(notificationService,Mockito.times(1))
                     .sendNotification("22222","Снятие средств успешно выполнено");
         }
+
+    @Test
+    @DisplayName("Снятие неудачно")
+    public void testProcessTransaction_Withdrawal_InsufficientFunds() {
+        BalanceService balanceService = Mockito.mock(BalanceService.class);
+        NotificationService notificationService = Mockito.mock(NotificationService.class);
+
+        TransactionProcessor transactionProcessor = new TransactionProcessor(balanceService,notificationService);
+
+        String accountId = "11111";
+        double amount = 11000.0;
+
+        Mockito.when(balanceService.hasSufficientFunds(accountId, amount))
+                .thenReturn(false);
+
+        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class,
+                () -> { transactionProcessor.processTransaction(accountId, new Transaction(amount, TransactionType.WITHDRAWAL));});
+
+        Mockito.verify(notificationService,Mockito.never())
+                .sendNotification(accountId, "Снятие средств успешно выполнено");
+    }
 }
